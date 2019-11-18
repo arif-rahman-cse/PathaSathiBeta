a18b45d1746a264ac500718ed11295ddc15fa433
arif_rahman_cse <smrahmanarif905@gmail.com>
2019-11-18 18:11:37 +0600

    drawer icon added

---
+++ app/src/main/java/com/example/pathasathi/activity/MakePathasathi.java
@@ -0,0 +1,16 @@
+package com.example.pathasathi.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pathasathi.R;
+
+public class MakePathasathi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_pathasathi);
+    }
+}
